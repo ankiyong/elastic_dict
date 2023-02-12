0b2966f73f4e577037668a82813487e6d047cb13
안기용 <angiyong@angiyong-ui-MacBookAir.local>
2023-02-12 21:18:09 +0900

    first

--- get_index.py
+++ get_index.py
@@ -6,19 +6,16 @@ class index_info:
         self.host = host
         self.username = username
         self.password = password
+
     def get_index(self):
-        host = self.host
-        username = self.username
-        password = self.password
+        host = self.host;username = self.username;password = self.password
         es = Elasticsearch(host, http_auth=(username,password), verify_certs=False)
         result = es.indices.get(index="*")
         index_list = list(result.keys())
         return result,index_list
 
 
-# es = Elasticsearch("https://localhost:9200", http_auth=('elastic','bGzOBS9el+QJ+XwoDDLM'), verify_certs=False)
 node1 = index_info("https://localhost:9200","elastic","bGzOBS9el+QJ+XwoDDLM")
-
 print(node1.get_index())
 
 
